842b4dea543ab43c757f80283d74db88258c192d
yk43zoo <saimomo4179@yahoo.co.jp>
2016-03-13 23:25:35 +0900

    jsonファイル出力処理追加

--- src/main/java/com/example/CreateObject.java
+++ src/main/java/com/example/CreateObject.java
@@ -1,6 +1,11 @@
 package com.example;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Date;
@@ -15,17 +20,49 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import com.example.test.dto.AppsInpt;
 import com.example.test.dto.HatApps;
 import com.example.test.dto.RowData;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CreateObject {
 
 	private static final String FILE_NAME = "data.xlsx";
 	private static final String SHEET_NAME = "Sheet1";
 
+	private static final String JSON_FILE_NAME = "testdata.json";
+
 	private static final int HAT_APPS_ROW_START = 3;
 	private static final int HAT_APPS_COLUMN_START = 2;
 
 	public static void main(String[] args) throws Exception {
 
+		
+		// Excelからデータを読み込みオブジェクトを作成する
+		
+		// Jsonを作成する
+		String json = createJson();
+
+		// Json文字列をファイルに出力する。
+		outputFile(json);
+		
+		// Jsonファイルからデータを読み込む
+		ObjectMapper mapper = new ObjectMapper();
+		File file = new File(JSON_FILE_NAME);
+		HatApps hatApps = mapper.readValue(file, HatApps.class);
+		
+		System.out.println(hatApps);
+
+	};
+
+	private static void outputFile(String json) throws Exception {
+		// ファイルに出力する
+		File file = new File(JSON_FILE_NAME);
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+		pw.println(json);
+		pw.flush();
+		pw.close();
+
+	}
+
+	private static String createJson() throws Exception {
 		CreateObject createObject = new CreateObject();
 
 		// AppsInpt
@@ -35,79 +72,81 @@ public class CreateObject {
 		// HatApps
 		List<RowData> listHatApps = createObject.readExcel(2, 20, 1);
 		HatApps hatApps = createObject.createObjectHatApps(listHatApps, appsInpt);
-		
-		
+
 		System.out.println(hatApps);
 
+		// オブジェクトをjsonに変換する
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(hatApps);
+		System.out.println("jsonデータ：" + json);
 
-	};
+		return json;
 
-	
+	}
+
 	private HatApps createObjectHatApps(List<RowData> dataList, AppsInpt appsInpt) throws Exception {
 
 		HatApps hatApps = new HatApps();
 
 		hatApps.setAppsInpt(appsInpt);
-		
+
 		Class clsHatApps = hatApps.getClass();
 
 		for (RowData data : dataList) {
-			
-			//String
+
+			// String
 			if ("String".equals(data.getKata())) {
 				Method m1 = clsHatApps.getMethod("set" + firstCharConvUpper(data.getFieldName()), String.class);
 				m1.invoke(hatApps, data.getValue());
 			}
-			
+
 			// List<String>
 			if ("List<String".equals(data.getKata())) {
 				Method m2 = clsHatApps.getMethod("set" + firstCharConvUpper(data.getFieldName()), List.class);
 				List<String> l = new ArrayList<>();
 				String[] ss = data.getValue().split(",");
 				for (String s : ss) {
-					l.add(s);					
+					l.add(s);
 				}
 				m2.invoke(hatApps, l);
-				
+
 			}
-			
+
 			// Date
 			if ("Date".equals(data.getKata())) {
 				Method m3 = clsHatApps.getMethod("set" + firstCharConvUpper(data.getFieldName()), Date.class);
 				m3.invoke(hatApps, new Date(data.getValue()));
 			}
-		}		
-		
+		}
+
 		return hatApps;
-		
-		
+
 	}
-	
+
 	private AppsInpt createObjectAppsInpt(List<RowData> dataList) throws Exception {
 
-		
 		AppsInpt appsInpt = new AppsInpt();
 
 		Class clsAppsInpt = appsInpt.getClass();
 
 		for (RowData data : dataList) {
-			
-			//�@String
+
+			// �@String
 			if ("String".equals(data.getKata())) {
 				Method m1 = clsAppsInpt.getMethod("set" + firstCharConvUpper(data.getFieldName()), String.class);
 				m1.invoke(appsInpt, data.getValue());
 			}
-			
+
 			// List<String>
 			if ("List<String".equals(data.getKata())) {
 				Method m2 = clsAppsInpt.getMethod("set" + firstCharConvUpper(data.getFieldName()), List.class);
 				List<String> l = new ArrayList<>();
 				String[] ss = data.getValue().split(",");
 				for (String s : ss) {
-					l.add(s);					
+					l.add(s);
 				}
 				m2.invoke(appsInpt, l);
-				
+
 			}
 		}
 
@@ -162,8 +201,10 @@ public class CreateObject {
 	}
 
 	public static String getStr(Cell cell) { // �f�[�^�^���̓ǂݎ��
-		if (cell ==null) {return "";}
-		
+		if (cell == null) {
+			return "";
+		}
+
 		switch (cell.getCellType()) {
 		case Cell.CELL_TYPE_BOOLEAN:
 			return Boolean.toString(cell.getBooleanCellValue());
@@ -179,16 +220,17 @@ public class CreateObject {
 
 	/**
 	 * 先頭文字を大文字に変更
+	 * 
 	 * @param s
 	 * @return
 	 */
 	public String firstCharConvUpper(String s) {
-		
+
 		if (s == null) {
 			return null;
 		}
-		
+
 		return s.substring(0, 1).toUpperCase() + s.substring(1);
 	}
-	
+
 }
